7340f2bc7aee2559fedf237d4d0fe4891164c06f
hamonangann <hamonangan.nito@gmail.com>
2021-12-29 18:59:19 +0700

    allow setting

--- efek_samping/lib/main.dart
+++ efek_samping/lib/main.dart
@@ -5,9 +5,9 @@ import 'home.dart';
 import 'lapor.dart';
 import 'laporan.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
